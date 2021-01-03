6197c2bff811743cf3a5bbf7921ce7a8ce2dfa86
Stephen Nygard <svcnygard@yahoo.com>
2021-01-02 20:59:33 -0600

    updated readme and python files

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# Unit---19---Multi-Blockchain-Wallet-in-Python
+# Unit - 19 -Multi-Blockchain-Wallet-in-Python
+
+This code creates a 'universal' wallet that manages multiple crypto coins allowing for portfolio-like management of multiple coins. For this test, I used Ethereum and Bitcoin Testnet. This code uses a command line tool, hd-wallet-derive that supports BIP32, BIP39, and BIP44 as well as non-standard derivation paths.
+
+# Adding HD-Wallet-Derive
+PHP should be installed for hd-wallet-derive to work.  The hd-wallet-derive library is written in the PHP language.  Install hd-wallet-derive by:
+1. Open a fresh terminal as *Administrator*.
+    - Input "C:\Program Files\Git\bin\bash.exe directly into the system search bar and launch as *Administrator*.
+
+2. With your terminal open, cd into your 'Blockchain-Tools' folder and run the following code:
+  - git clone https://github.com/dan-da/hd-wallet-derive
+  - cd hd-wallet-derive
+  - curl https://getcomposer.org/installer -o installer.php
+  - php installer.php
+  - php composer.phar install
+
+### You should now have a folder called hd-wallet-derive. Verify that it works by:
+
+3. Run the command to cd into hd-wallet-derive
+4. Execute the following command:
+  - ./hd-wallet-derive.php -g --key=xprv9tyUQV64JT5qs3RSTJkXCWKMyUgoQp7F3hA1xzG6ZGu6u6Q9VMNjGr67Lctvy5P8oyaYAL9CAWrUE9i6GoNMKUga5biW6Hx4tws2six3b9c --numderive=3 --preset=bitcoincore --cols=path,address --path-change
+
+ 5. Once verified open *Administrator* Bash and cd to Block-chain tools and runt he following:
+
+  - export MSYS=winsymlinks:nativestrict
+  - ln -s hd-wallet-derive/hd-wallet-derive.php derive
+
+  To verify it's working type the following two commands into your command line:
+  - export MSYS=winsymlinks:nativestrict
+  - ln -s hd-wallet-derive/hd-wallet-derive.php derive
+
+  Test that it's working by entering:
+  - ./derive -g --mnemonic="YOU'RE MNEMONIC HERE" --cols=path,address,privkey,pubkey
+
+  Here are [instructions](requirement.txt) on how to install the additional dependencies.
+
+# Running wallet.py
+### This function is built to send ETH and BTCTEST coins:
+
+    def send_tx(coin, account, recipient, amount):
+      tx = create_raw_tx(coin, account, recipient, amount)
+      if coin == ETH:
+          print(w3.eth.getBalance(eth_account.address))
+          try:
+            signed_tx = account.sign_transaction(tx)
+            return print(w3.eth.sendRawTransaction(signed_tx.rawTransaction))
+          except:
+            return print ("Was not able to send" + f"{coin} coins.")
+      if coin == BTCTEST:
+          print(account.get_balance('btc'))
+          try:
+            signed_tx = account.sign_transaction(tx)
+            NetworkAPI.broadcast_tx_testnet(signed_tx)
+            transaction = account.get_transactions()[0]
+            return print("Your Transaction Hash is : " + f"{transaction}.")
+        except:
+            return print ("Was not able to send" + f"{coin} coins.")
+
+### To send Ether, use the following command:
+
+    send_tx(ETH, eth_account, send_eth, 3)
+
+![Test Ether Confirmation](Screenshots/eth.png)
+
+### To send TEST BTC, use the following command:
+
+    send_tx(BTCTEST, btctest_account, send_bit, 0.0000001)
+
+![Test BTC Confirmation](Screenshots/bit_testnet_tx.png)
+
+# IV. Next steps 
+Next is to code for other coin types to increase the functionality of the wallet.
+
+# I. Dependencies
+- subprocess
+- json
+- os
+- constants.py
+- bit
+- web3
--- wallet.py
+++ wallet.py
@@ -1,192 +1,148 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # Dependencies
-import subprocess
-import json
-import os
+ import subprocess 
+ import json 
+ import os 
+  
+ from constants import BTC, BTCTEST, ETH 
+ from pprint import pprint 
+  
+ from bit import PrivateKeyTestnet 
+ from bit.network import NetworkAPI 
+  
+ from web3 import Web3, middleware, Account 
+ from web3.gas_strategies.time_based import medium_gas_price_strategy 
+ from web3.middleware import geth_poa_middleware
 
-from constants import BTC, BTCTEST, ETH
-from pprint import pprint
+ from eth_account import Account 
+ # Connect Web3 
+ w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER", "http://localhost:8545"))) 
+ w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+ w3.middleware_onion.inject(geth_poa_middleware, layer=0) 
+  
+ # Current block number 
+ w3.eth.blockNumber 
+  
+ # Set Gas Price Strategy 
+ w3.eth.setGasPriceStrategy(medium_gas_price_strategy) 
+  
+ # Mnemonic Phrase 
+ mnemonic = os.getenv("MNEMONIC" , "abuse blood note choice garbage spot ice roast smoke yard foster chuckle')
 
-from bit import PrivateKeyTestnet
-from bit.network import NetworkAPI
+ coin = ETH 
+ mnemonic=mnemonic 
+ depth=3 
+  
+ command = f'php derive -g --mnemonic={mnemonic} --coin={coin} --numderive={depth} --format=json' 
+ p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True) 
+ (output, err) = p.communicate() 
+ pprint(output) 
 
-from web3 import Web3, middleware, Account
-from web3.gas_strategies.time_based import medium_gas_price_strategy
-from web3.middleware import geth_poa_middleware
+ coin=BTCTEST 
+ mnemonic=mnemonic 
+ depth=3 
+  
+ command = f'php derive -g --mnemonic=\ {mnemonic}\  --coin={coin}  --numderive={depth} --format=json'  ,
+ p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+ (output, err) = p.communicate()
+ keys = json.loads(output)
+ pprint(keys)
 
-from eth_account import Account
+ coin=BTC
+ mnemonic=mnemonic
+ depth=3
 
+ command = f'php derive -g --mnemonic={mnemonic} --coin={coin} --numderive={depth} --format=json'  ,
+ p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)  ,
+ (output, err) = p.communicate()
+ p_status = p.wait()
+ keys = json.loads(output)
+ pprint(keys) 
 
-# In[2]:
+ def derive_wallets(coin=BTC, mnemonic=mnemonic, depth=3):
+     command = f'php derive -g --mnemonic=\ {mnemonic}\  --coin={coin}  --numderive={depth} --format=json'
 
+     p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True) 
 
-# Connect Web3
-w3 = Web3(Web3.HTTPProvider(os.getenv('WEB3_PROVIDER', 'http://localhost:8545')))
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
-w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+     (output, err) = p.communicate()
+     p_status = p.wait()
 
-# Current block number
-w3.eth.blockNumber
+     return json.loads(output) 
 
-# Set Gas Price Strategy
-w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+ coins = {ETH: derive_wallets(coin=ETH), BTCTEST: derive_wallets(coin=BTCTEST)}
+ pprint(coins) 
 
-# Mnemonic Phrase
-mnemonic = os.getenv("MNEMONIC", 'abuse blood note choice garbage spot ice roast smoke yard foster chuckle')
+ def priv_key_to_account(coin, priv_key):
+     if coin == ETH:
+         return Account.privateKeyToAccount(priv_key)
+     if coin == BTCTEST:
+         return PrivateKeyTestnet(priv_key) 
 
+ # Use the private key to send transactions
+ private_key_eth = coins['eth'][0]['privkey']
+ private_key_btctest = coins['btc-test'][0]['privkey'] 
 
-# In[3]:
+ # ETH account address saved
+ eth_account = priv_key_to_account(ETH, private_key_eth)
+ print(eth_account.address)
 
+ # Address balance eth
+ print(w3.eth.getBalance(eth_account.address))
+ eth_account 
 
-coin = ETH
-mnemonic=mnemonic
-depth=3
+ # BTCTEST account address saved
+ btctest_account = priv_key_to_account(BTCTEST, private_key_btctest)
+ print(btctest_account.address)
 
-command = f'php derive -g --mnemonic={mnemonic} --coin={coin} --numderive={depth} --format=json'
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-(output, err) = p.communicate()
-print(output)
+ # Address balance btc-test
+ print(btctest_account.get_balance('btc'))
+ btctest_account 
 
+ # Send btc to this account address
+ send_bit = coins['btc-test'][1]['address']
+ print(send_bit) 
 
-# In[4]:
+ # Send eth to this account address (yourself)
+ send_eth = coins['eth'][0]['address']
+ print(send_eth) 
 
-
-coin='btc-test'
-mnemonic=mnemonic
-depth=3
-
-command = f'php derive -g --mnemonic="{mnemonic}" --coin={coin}  --numderive={depth} --format=json'
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-(output, err) = p.communicate()
-keys = json.loads(output)
-print(keys)
-
-
-# In[5]:
-
-
-coin=BTC
-mnemonic=mnemonic
-depth=3
-
-command = f'php derive -g --mnemonic="{mnemonic}" --coin={coin}  --numderive={depth} --format=json'
-p = subprocess.Popen(
-    command,
-    stdout=subprocess.PIPE,
-    shell=True)
-(output, err) = p.communicate()
-p_status = p.wait()
-keys = json.loads(output)
-print(keys)
-
-
-# In[6]:
-
-
-def derive_wallets(coin=BTC, mnemonic=mnemonic, depth=3):
-    command = f'php derive -g --mnemonic="{mnemonic}" --coin={coin}  --numderive={depth} --format=json'
-
-    p = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        shell=True)
+  def create_raw_tx(coin, account, recipient, amount):
+         global tx_data
+         if coin == ETH:
+             gasEstimate = w3.eth.estimateGas(
+                 {from : account.address, to : recipient,  value : amount}
+             )
+             tx_data = {
+                 from : account.address,
+                 to : recipient,
+                 value : amount,
+                 gasPrice : w3.eth.gasPrice,
+                 gas : gasEstimate,
+                 nonce : w3.eth.getTransactionCount(account.address)
+             }
+             return tx_data
     
-    (output, err) = p.communicate()
-    p_status = p.wait()
-    
-    return json.loads(output)
+         if coin == BTCTEST:
+             return PrivateKeyTestnet.prepare_transaction(account.address, [(recipient, amount, BTC)]) 
 
+ def send_tx(coin, account, recipient, amount):
+     tx = create_raw_tx(coin, account, recipient, amount)
+     if coin == ETH:
+         print(w3.eth.getBalance(eth_account.address))
+         try:
+             signed_tx = account.sign_transaction(tx)
+             return print(w3.eth.sendRawTransaction(signed_tx.rawTransaction))
+         except:
+             return print ("Was not able to send"  + f"{coin} coins.")
+     if coin == BTCTEST:
+         print(account.get_balance('btc'))
+         try:
+             signed_tx = account.sign_transaction(tx)
+             NetworkAPI.broadcast_tx_testnet(signed_tx)
+             transaction = account.get_transactions()[0]
+             return print("Your Transaction Hash is :"  + f"{transaction}.")
+         except:
+             return print ("Was not able to send"  + f"{coin} coins.") 
 
-# In[7]:
+ #send_tx(ETH, eth_account, send_eth, 3) 
 
-
-coins = {
-    ETH: derive_wallets(coin=ETH),
-    BTCTEST: derive_wallets(coin=BTCTEST),
-}
-pprint(coins)
-
-
-# In[8]:
-
-
-def priv_key_to_account(coin, priv_key):
-    if coin == ETH: 
-        return Account.privateKeyToAccount(priv_key)
-    if coin == BTCTEST:
-        return PrivateKeyTestnet(priv_key)
-
-
-# In[9]:
-
-
-# Use the private key to send transactions
-private_key = coins['eth'][0]['privkey']
-
-
-# In[10]:
-
-
-# Account One address saved
-account_one = priv_key_to_account(ETH, private_key)
-print(account_one.address)
-account_one
-
-
-# In[12]:
-
-
-# Account Two address saved
-account_two = Account.from_key(private_key)
-print(account_two.address)
-account_two
-
-
-# In[20]:
-
-
-# Address balance
-w3.eth.getBalance("0xE8871046ea8Ad02D624b8A46b85fBE3A9f4CDD95")
-
-# Send eth to this account address
-to = "0xE8871046ea8Ad02D624b8A46b85fBE3A9f4CDD95"
-
-
-# In[21]:
-
-
-def create_raw_tx(account, recipient, amount):
-   gasEstimate = w3.eth.estimateGas(
-       {"from": account.address, "to": recipient, "value": amount}
-   )
-   return {
-       "from": account.address,
-       "to": recipient,
-       "value": amount,
-       "gasPrice": w3.eth.gasPrice,
-       "gas": gasEstimate,
-       "nonce": w3.eth.getTransactionCount(account.address),
-   }
-
-
-# In[22]:
-
-
-def send_tx(account, recipient, amount):
-    tx = create_raw_tx(account, recipient, amount)
-    signed_tx = account.sign_transaction(tx)
-    result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    print(result.hex())
-    return result.hex()
-
-
-# In[23]:
-
-
-send_tx(account_one, to, 3)
-
+ #send_tx(BTCTEST, btctest_account, send_bit, 0.0000001) 
